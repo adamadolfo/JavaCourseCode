88a46bba8677b1ac72b0ad8f2fbc7a9a37eef745
Adam <Adam@DESKTOP-FPMQAVG.fios-router.home>
2021-02-02 11:32:46 -0500

    fsfsdf

--- JavaCourse/src/HelloWorld.java
+++ JavaCourse/src/HelloWorld.java
@@ -8,10 +8,20 @@
 //	}
 //}
  
+//public class HelloWorld {
+//	public static void main(String[] args) {
+//		System.out.println("test");
+//		String stringVariable = "hi";
+//		System.out.println(stringVariable);
+//	}
+//}
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("test");
 		String stringVariable = "hi";
 		System.out.println(stringVariable);
+		System.out.println("test1");
+		System.out.println("test2");
 	}
 }
