8ca2f1fbc8560c2fc1df427d9d2bbac03581257d
Adam <Adam@DESKTOP-FPMQAVG.fios-router.home>
2021-02-02 11:23:22 -0500

    class print and variable review

--- JavaCourse/src/HelloWorld.java
+++ JavaCourse/src/HelloWorld.java
@@ -1,9 +1,17 @@
-
+//
+//public class HelloWorld {
+//	public static void main(String[] args) {
+//		System.out.println("hello world");
+//		
+//		int myFirstNum = 5;
+//		System.out.println(myFirstNum);
+//	}
+//}
+ 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		
-		int myFirstNum = 5;
-		System.out.println(myFirstNum);
+		System.out.println("test");
+		String stringVariable = "hi";
+		System.out.println(stringVariable);
 	}
 }
